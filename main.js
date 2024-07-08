da558289e82b5c4f55eefedb305580ac01fb6e24
dawon jeong <dwnee0@gmail.com>
2024-07-08 17:11:02 +0900

    retry

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const API_KEY = `b3890c85e6f94350bca0576090671f11`;
+// const API_KEY = `b3890c85e6f94350bca0576090671f11`;
 let keyword = '';
 let news = [];
 const getLatestNews = async ()=>{
-  const url = new URL(`https://noona-times-2024july.netlify.app/`);
+  const url = new URL(`https://noona-times-2024july.netlify.app/top-headlines?page=1&pageSize=20`);
   console.log(url);
   const response = await fetch(url);
   const data = await response.json();
