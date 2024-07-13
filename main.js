cc38fb4a3776ebd31643b6d0b5bee9ef2269d625
dawon jeong <dwnee0@gmail.com>
2024-07-13 15:27:37 +0900

    pagination 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const API_KEY = `b3890c85e6f94350bca0576090671f11`;
+const API_KEY = `b3890c85e6f94350bca0576090671f11`;
 // const API_KEY = `b3890c85e6f94350bca0576090671f1`; // error test
 let mode = "tab-all"
 let newsList = [];
@@ -7,19 +7,29 @@ const menus = document.querySelectorAll(".menus button");
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)));
 const mobileMenus = document.querySelectorAll(".side-menu-list button");
 mobileMenus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)));
+// let url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 
 const getNews = async() =>{
   try{
+    url.searchParams.set("page", page); // => &page=page
+    url.searchParams.set("pageSize", pageSize);
     const response = await fetch(url);
     const data = await response.json();
+    console.log("ddd", data)
     if(response.status===200){
       if(data.articles.length===0){
         throw new Error("No result for this research");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     }else{
       throw new Error(data.message)
     }
@@ -30,23 +40,29 @@ const getNews = async() =>{
 }
 
 const getLatestNews = async() => {
+  // url = new URL(
+  //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  // );
   url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`
   );
-  getNews();
+
+  await getNews();
 }
 
 const getNewsByCategory= async (event)=>{
   const category = event.target.textContent.toLowerCase();
   console.log("click category button", category)
+  // url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`);
   url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`);
-  getNews();
+  await getNews();
 }
 
 const searchNews = async() => {
   const inputValue = document.getElementById('search-input').value;
+  // url = new URL(`https://newsapi.org/v2/top-headlines?q=${inputValue}&country=us&apiKey=${API_KEY}`);
   url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${inputValue}`);
-  getNews();
+  await getNews();
 }
 
 const render=()=>{
@@ -88,7 +104,6 @@ document.getElementById('search-input').addEventListener('click', function() {
   this.value = "";
 });
 
-getLatestNews();
 
 /* search box open */
 const openSearchBox = () => {
@@ -101,10 +116,55 @@ const openSearchBox = () => {
     console.log("else inputarea")
   }
 }
-/* mobile hamburger button category menu open close */
+
+/* 모바일 메뉴 햄버거 버튼 오픈 클로즈 */
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
 };
 const closeNav = () => {
   document.getElementById("mySidenav").style.width = "0"
 }
+
+const paginationRender=()=>{
+  // totalResult
+  // page
+  // pageSize
+  // groupSize
+  // totalPages
+  const totalPages = Math.ceil(totalResults/pageSize);
+  // pageGroup
+    const pageGroup = Math.ceil(page/groupSize);
+  // lastPage
+    let lastPage = pageGroup * groupSize;
+    // 마지막 페이지 그룹이 그룹 사이즈보다 작다? lastpage = total page
+    if(lastPage > totalPages){
+      lastPage = totalPages;
+    }
+
+  // firstPage
+    const firstPage = lastPage - (groupSize - 1)<=0?1:lastPage - (groupSize - 1);
+
+  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`;
+  for(let i=firstPage;i<=lastPage;i++){
+    paginationHTML += `<li class="page-item ${i===page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+  }
+  paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+
+  /* <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav> */
+}
+const moveToPage = async (pageNum)=>{
+  console.log("movetopage", pageNum);
+  page = pageNum;
+  await getNews();
+}
+
+getLatestNews();
